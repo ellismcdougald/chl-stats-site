04a37c85128b152dc9ed1ca6102b65b0fc288242
Ellis McDougald <ecmcdougald@gmail.com>
2023-08-20 16:06:09 -0600

    Adds footer to layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../styles/Footer.module.css";
+
+export default function Footer() {
+  return (
+    <div id={styles.container}>
+      <p>Website by Ellis McDougald | Statistics from chl.ca</p>
+    </div>
+  );
+}
