03c9c118176ba71feaf619a657991fa30dfda351
Ellis McDougald <ecmcdougald@gmail.com>
2023-09-04 10:59:31 -0600

    Adds CORS to app

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/stats",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
